63c2f3d5de102f952ce1cb532f9faf9c731c8eeb
Tao <422618856@qq.com>
2020-02-21 19:41:57 +0800

    update

--- golang/cache/redis/redis-interface.go
+++ golang/cache/redis/redis-interface.go
@@ -5,30 +5,29 @@ import "github.com/gomodule/redigo/redis"
 type Cache interface {
 	Encode() string
 	Decode(reply string) error
-	Key() string
 	RedisConn() redis.Conn
 	Exp() int64
 }
 
-func SetCache(c Cache) error {
+func SetCache(c Cache, key string) error {
 	conn := c.RedisConn()
 	if conn.Err() != nil {
 		return conn.Err()
 	}
 	defer conn.Close()
 
-	_, err := redis.String(conn.Do("setex", c.Key(), c.Exp(), c.Encode()))
+	_, err := redis.String(conn.Do("setex", key, c.Exp(), c.Encode()))
 	return err
 }
 
-func GetCache(c Cache) error {
+func GetCache(c Cache, key string) error {
 	conn := c.RedisConn()
 	if conn.Err() != nil {
 		return conn.Err()
 	}
 	defer conn.Close()
 
-	reply, err := redis.String(conn.Do("get", c.Key()))
+	reply, err := redis.String(conn.Do("get", key))
 	if err != nil || reply == "" {
 		return err
 	}
